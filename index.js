ce1474afaea177d99ca436a7334c9e01b387bee9
AryBacher <47170979@est.ort.edu.ar>
2022-09-13 08:39:08 -0300

    feat: Initial Commit

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ console.log("Hello")
 for (var i = 0; i < 9; i++) {
     console.log();
 }
+
+// Git en Powershell
+// git init - Para Iniciar
+// git status - Para ver el Estado
+// git branch -M main - Cambiamos de master a main
+// git remote add origin https://github.com/AryBacher/Clase-Node-1
+// git add (filename)
+// git commit -m "feat: first commit"
+// git push
+// git push origin main
+// git pull origin main
